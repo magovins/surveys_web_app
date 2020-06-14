5f850fc902e7bbbfff13ad9b90b10bfe3edf8b4e
Bota Vicentiu <vladutvicentiu.bota@studio.unibo.it>
2020-06-14 07:39:31 +0200

    Welcome page title modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Survey App
+                    Survey Web App
                 </div>
 
                 <div class="links">
